293638a0b946cc049a9a049314342742f8dc575d
илья бугай <bugai.samara@yandex.ru>
2022-11-29 18:34:45 +0400

    Добавление функций репозитория Adding Repository functions

--- app/src/main/java/com/example/mynotesroom/NoteRepository.kt
+++ app/src/main/java/com/example/mynotesroom/NoteRepository.kt
@@ -2,7 +2,9 @@ package com.example.mynotesroom
 
 import android.content.Context
 import androidx.room.Room
+import com.example.mynotesroom.model.Notes
 import com.example.mynotesroom.model.database.NoteDataBase
+import java.util.*
 
 
 private const val DB_NAME = "note_db"
@@ -14,7 +16,13 @@ class NoteRepository private constructor(context: Context) {
         NoteDataBase::class.java,
         DB_NAME
     ).build()
-private val noteDao = db.noteDao()
+    private val noteDao = db.noteDao()
+
+
+    fun getNotes(): List<Notes> = noteDao.getNotes()
+
+    fun getNote(id: UUID): Notes = noteDao.getNote(id)
+
     companion object {
         private var INSTANCE: NoteRepository? = null
 
@@ -24,7 +32,7 @@ private val noteDao = db.noteDao()
             }
         }
 
-        fun get():NoteRepository = INSTANCE?: throw
+        fun get(): NoteRepository = INSTANCE ?: throw
         IllegalStateException("CrimeRepository must be initialized")
     }
 }
