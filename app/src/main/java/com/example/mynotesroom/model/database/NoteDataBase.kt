534bf80c9b274f93c5e6d5698b5d8b9ae4c83ab1
илья бугай <bugai.samara@yandex.ru>
2022-11-29 18:08:40 +0400

    Declaration of conversion via database annotations Объявление конвертации через аннотации database

--- app/src/main/java/com/example/mynotesroom/model/database/NoteDataBase.kt
+++ app/src/main/java/com/example/mynotesroom/model/database/NoteDataBase.kt
@@ -2,9 +2,11 @@ package com.example.mynotesroom.model.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.mynotesroom.model.Notes
 
 @Database(entities = [Notes::class], version = 1)
+@TypeConverters(NoteTypeConverter::class)
 abstract class NoteDataBase() : RoomDatabase() {
     abstract fun noteDao(): Notes
 }
