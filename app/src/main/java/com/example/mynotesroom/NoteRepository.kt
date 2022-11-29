2421056dc712e375a2bd5b2ae4fcff0dcb4ff4ba
илья бугай <bugai.samara@yandex.ru>
2022-11-29 18:06:20 +0400

    This is a singleton class (singleton) Это одноэлементный класс (синглтон)

--- app/src/main/java/com/example/mynotesroom/NoteRepository.kt
+++ app/src/main/java/com/example/mynotesroom/NoteRepository.kt
@@ -1,4 +1,19 @@
 package com.example.mynotesroom
 
-class NoteRepository {
+import android.content.Context
+
+class NoteRepository private constructor(context: Context) {
+
+    companion object {
+        private var INSTANCE: NoteRepository? = null
+
+        fun init(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = NoteRepository(context)
+            }
+        }
+
+        fun get():NoteRepository = INSTANCE?: throw
+        IllegalStateException("CrimeRepository must be initialized")
+    }
 }
